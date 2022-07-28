a5cad27ce48195109583f4c38005a991c084a179
Bikcode <bikcodeh@gmail.com>
2022-07-27 22:53:06 -0500

    initial setup for dog list and display harcoded data
    -create dog adapter
    -add recyclerview

---
+++ app/src/main/java/com/bikcodeh/dogrecognizer/domain/model/Dog.kt
@@ -0,0 +1,15 @@
+package com.bikcodeh.dogrecognizer.domain.model
+
+data class Dog(
+    val id: Long,
+    val index: Int,
+    val name: String,
+    val type: String,
+    val heightFemale: Double,
+    val heightMale: Double,
+    val imageUrl: String,
+    val lifeExpectancy: String,
+    val temperament: String,
+    val weightFemale: Double,
+    val weighMale: Double
+)
---
+++ app/src/main/java/com/bikcodeh/dogrecognizer/presentation/doglist/DogListAdapter.kt
@@ -0,0 +1,39 @@
+package com.bikcodeh.dogrecognizer.presentation.doglist
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bikcodeh.dogrecognizer.databinding.ItemDogBinding
+import com.bikcodeh.dogrecognizer.domain.model.Dog
+
+class DogListAdapter : ListAdapter<Dog, DogListAdapter.DogViewHolder>(DiffCallback()) {
+
+    private class DiffCallback : DiffUtil.ItemCallback<Dog>() {
+        override fun areItemsTheSame(oldItem: Dog, newItem: Dog): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Dog, newItem: Dog): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    inner class DogViewHolder(private val binding: ItemDogBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(dog: Dog) {
+            binding.dogNameTv.text = dog.name
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DogViewHolder {
+        val binding = ItemDogBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return DogViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: DogViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
