62871514728f8302a8d56bda383a38ce4792b436
Bikcode <bikcodeh@gmail.com>
2022-07-29 07:58:38 -0500

    add dog detail activity
    -add styles
    -add strings
    -add drawables
    -add colors

---
+++ app/src/main/java/com/bikcodeh/dogrecognizer/presentation/dogdetail/DogDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.bikcodeh.dogrecognizer.presentation.dogdetail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bikcodeh.dogrecognizer.R
+
+class DogDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dog_detail)
+    }
+}
