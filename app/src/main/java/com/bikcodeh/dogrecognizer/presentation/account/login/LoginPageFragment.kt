93761f63072da7ba0272262dbe59034345468437
Bikcode <bikcodeh@gmail.com>
2022-07-30 10:59:05 -0500

    implements navigate to sign up from login and add transitions

--- app/src/main/java/com/bikcodeh/dogrecognizer/presentation/account/login/LoginPageFragment.kt
+++ app/src/main/java/com/bikcodeh/dogrecognizer/presentation/account/login/LoginPageFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.bikcodeh.dogrecognizer.R
 import com.bikcodeh.dogrecognizer.databinding.FragmentLoginPageBinding
 
@@ -25,5 +26,16 @@ class LoginPageFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpListeners()
+    }
+
+    private fun setUpListeners() {
+        binding.loginRegisterButton.setOnClickListener {
+            findNavController().navigate(R.id.action_loginPageFragment_to_signUpFragment)
+        }
     }
 }
--- app/src/main/java/com/bikcodeh/dogrecognizer/presentation/account/signup/SignUpFragment.kt
+++ app/src/main/java/com/bikcodeh/dogrecognizer/presentation/account/signup/SignUpFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.bikcodeh.dogrecognizer.R
 import com.bikcodeh.dogrecognizer.databinding.FragmentSignUpBinding
 
 class SignUpFragment : Fragment() {
@@ -13,11 +16,25 @@ class SignUpFragment : Fragment() {
     private val binding: FragmentSignUpBinding
         get() = _binding!!
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activity?.onBackPressedDispatcher?.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                findNavController().popBackStack()
+            }
+        })
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentSignUpBinding.inflate(layoutInflater, container, false)
+        activity?.setActionBar(binding.signUpToolbar)
+        activity?.actionBar?.setDisplayHomeAsUpEnabled(true)
+        binding.signUpToolbar.setNavigationOnClickListener {
+            findNavController().popBackStack()
+        }
         return binding.root
     }
 
