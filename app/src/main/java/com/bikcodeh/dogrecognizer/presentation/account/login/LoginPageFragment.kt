430d4fb77c3e798f9aad1a669a46bb610362ab6f
Bikcode <bikcodeh@gmail.com>
2022-07-30 10:13:22 -0500

    setup view binding on fragments for login

--- app/src/main/java/com/bikcodeh/dogrecognizer/presentation/account/login/LoginPageFragment.kt
+++ app/src/main/java/com/bikcodeh/dogrecognizer/presentation/account/login/LoginPageFragment.kt
@@ -6,14 +6,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.bikcodeh.dogrecognizer.R
+import com.bikcodeh.dogrecognizer.databinding.FragmentLoginPageBinding
 
 class LoginPageFragment : Fragment() {
+
+    private var _binding: FragmentLoginPageBinding? = null
+    private val binding: FragmentLoginPageBinding
+        get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        _binding = FragmentLoginPageBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
 
-        return inflater.inflate(R.layout.fragment_login_page, container, false)
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/com/bikcodeh/dogrecognizer/presentation/account/signup/SignUpFragment.kt
+++ app/src/main/java/com/bikcodeh/dogrecognizer/presentation/account/signup/SignUpFragment.kt
@@ -5,14 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.bikcodeh.dogrecognizer.R
+import com.bikcodeh.dogrecognizer.databinding.FragmentSignUpBinding
 
 class SignUpFragment : Fragment() {
+
+    private var _binding: FragmentSignUpBinding? = null
+    private val binding: FragmentSignUpBinding
+        get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    ): View {
+        _binding = FragmentSignUpBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
